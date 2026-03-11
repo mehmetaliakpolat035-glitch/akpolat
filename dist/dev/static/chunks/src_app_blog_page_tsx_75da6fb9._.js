96f801d69ad45d826e2c83d6eee4ff5a16be2ad7
guulAktop <gul.aktopp@gmail.com>
2026-03-12 01:47:29 +0300

    fix: revert to dist directory for Cloudflare Pages

--- dist/dev/static/chunks/src_app_blog_page_tsx_75da6fb9._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_ad050f6e._.js",
-  "static/chunks/node_modules_406ecb0e._.js"
-],
-    source: "dynamic"
-});
